d79344491ab48e47f1c351e7ecdc827882e2805e
Lidzy06 <108273346+Lidzy06@users.noreply.github.com>
2024-01-24 07:13:26 +0100

    2 itérations
    filigrane + compression

--- app/Http/Controllers/EcoleDoctorat/DossierController.php
+++ app/Http/Controllers/EcoleDoctorat/DossierController.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Storage;
 
 use setasign\Fpdi\Fpdi; 
 require_once(__DIR__ . '/../../../../vendor/autoload.php');
-
+use Ilovepdf\Ilovepdf;
 use TCPDF;
 
 class DossierController extends Controller
@@ -50,6 +50,126 @@ class DossierController extends Controller
             'selectedProject'=>$selectedProject
         ]);
     }
+
+    public function updateMemoirePdf($selectedProject)
+    {
+        set_time_limit(300);
+        // Source file and watermark config 
+            $file = "uploads/themes/{$selectedProject->theme}/memoire/{$selectedProject->memoire_path}"; 
+            $text_image = "assets/img/te-sea-4.png"; 
+            $text = 'Published by Te-sea publication'; 
+            $newPdfPath = public_path("uploads/themes/{$selectedProject->theme}/memoire/{$selectedProject->memoire_path}_modified.pdf");
+            
+            // Set source PDF file 
+
+            $name = uniqid(); 
+            $font_size = 3; 
+            $opacity = 100; 
+            $ts = explode("\n", $text); 
+            $width = 0; 
+            foreach($ts as $k=>$string){ 
+                $width = max($width, strlen($string)); 
+            } 
+            $width  = imagefontwidth($font_size)*$width; 
+            $height = imagefontheight($font_size)*count($ts); 
+            $el = imagefontheight($font_size); 
+            $em = imagefontwidth($font_size); 
+            $img = imagecreatetruecolor($width, $height); 
+
+            $bg = imagecolorallocate($img, 255, 255, 255); 
+            imagefilledrectangle($img, 0, 0, $width, $height, $bg); 
+ 
+            // Font color settings 
+            $color = imagecolorallocate($img, 0, 0, 0); 
+            foreach($ts as $k=>$string){ 
+                $len = strlen($string); 
+                $ypos = 0; 
+                for($i=0;$i<$len;$i++){ 
+                    $xpos = $i * $em; 
+                    $ypos = $k * $el; 
+                    imagechar($img, $font_size, $xpos, $ypos, $string, $color); 
+                    $string = substr($string, 1);       
+                } 
+            } 
+            imagecolortransparent($img, $bg); 
+            $blank = imagecreatetruecolor($width, $height); 
+            $tbg = imagecolorallocate($blank, 255, 255, 255); 
+            imagefilledrectangle($blank, 0, 0, $width, $height, $tbg); 
+            imagecolortransparent($blank, $tbg); 
+            $op = !empty($opacity)?$opacity:100; 
+            if ( ($op < 0) OR ($op >100) ){ 
+                $op = 100; 
+            } 
+            
+            // Create watermark image 
+            imagecopymerge($blank, $img, 0, 0, 0, 0, $width, $height, $op); 
+            imagepng($blank, $name.".png"); 
+
+            $pdf = new Fpdi(); 
+            if(file_exists("./".$file)){ 
+                $pagecount = $pdf->setSourceFile($file); 
+            }else{ 
+                die('Source PDF not found!'); 
+            } 
+            
+            // Add watermark image to PDF pages 
+            for($i=1;$i<=$pagecount;$i++){ 
+                $tpl = $pdf->importPage($i); 
+                $size = $pdf->getTemplateSize($tpl); 
+                $pdf->addPage(); 
+                $pdf->useTemplate($tpl, 1, 1, $size['width'], $size['height'], TRUE); 
+
+                $xxx_final = ($size['width']-60); 
+                $yyy_final = ($size['height']-8); 
+                $pdf->Image($name.'.png', $xxx_final, $yyy_final, 0, 0, 'png'); 
+                
+                //Put the watermark 
+                // list($imageWidth, $imageHeight) = getimagesize($text_image); // Obtenez les dimensions de l'image
+                // $xxx_final = ($size['width'] - $imageWidth) / 2; // Position X pour centrer l'image horizontalement
+                // $yyy_final = ($size['height'] - $imageHeight) / 2; // Position Y pour centrer l'image verticalement
+                // $pdf->Image($text_image, $xxx_final, $yyy_final, 0, 0, 'png'); 
+                $pdf->Image($text_image, 5, 5, 189); 
+            } 
+            @unlink($name.'.png');
+            // Output PDF with watermark 
+            $pdf->Output($newPdfPath, 'F');
+
+            // Compress the PDF with watermark using ilovepdf API
+            $ilovepdfPublicKey = "project_public_194707eee8c6bd80b6e1ff62702bcbe0_3d2wMd011adc7ecb22574c9c081e9f3552ade";
+            $ilovepdfSecretKey = "secret_key_0b46389dbf04b99020e9d5b5a4a92162_5CkIwf291d5e2527adf6c45503ec294200819";
+
+            $ilovepdf = new Ilovepdf($ilovepdfPublicKey, $ilovepdfSecretKey);
+
+            $task = $ilovepdf->newTask('compress');
+           
+            $task->addFile($newPdfPath);
+
+            $task->setOutputFilename('{filename}_compressed.pdf');
+
+            $task->execute();
+            // $task->setOutputFilename('{filename}_compressed');
+
+            // $compressedPdfContent = $task->download();
+
+    // Save the compressed PDF to a file
+            $compressedPdfPath = public_path("uploads/themes/{$selectedProject->theme}/memoire/");
+            $task->download($compressedPdfPath);
+            // $task->getResult()->download( $compressedPdfPath);
+            // file_put_contents($compressedPdfPath, $compressedPdfContent);
+
+            // Update the database with the path to the compressed PDF
+            $selectedProject->memoire_path = "{$selectedProject->memoire_path}_modified_compressed.pdf";
+            $selectedProject->save();
+
+            return $compressedPdfPath;
+
+            // // Mettre à jour le chemin du fichier dans la base de données
+            // $selectedProject->memoire_path = "{$selectedProject->memoire_path}_modified.pdf";
+            // $selectedProject->save();
+    
+            // return $newPdfPath;
+    }
+
     public function valider($id, Request $request){
         $selectedDossier = Projets::where('id',$id)->first();
 
